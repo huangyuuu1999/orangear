423ebb6dfe4c93f8bed78fc1e21ed5ce54f620ea
gongyulei <gongyulei@qianxin.com>
2023-01-17 20:30:52 +0800

    add element-ui

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,8 +1,24 @@
-<script>
+<script setup>
+import { ElMessage, ElMessageBox } from 'element-plus'
+
+const open = () => {
+  ElMessageBox.alert('您点击了按钮，展示此弹框', 'ElementUI-plus.', {
+    // if you want to disable its autofocus
+    // autofocus: false,
+    confirmButtonText: 'OK',
+    callback: (action) => {
+      ElMessage({
+        type: 'info',
+        message: `action: ${action}`,
+      })
+    },
+  })
+}
 </script>
 
 <template>
     <h1>About Page.</h1>
+    <el-button text @click="open">Click to open the Message Box</el-button>
 </template>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import './style.css'
 import App from './App.vue'
 import Home from './components/Home.vue'
 import About from './components/About.vue'
+import ElementUI from 'element-plus'
+import 'element-plus/dist/index.css'
+
 
 const routes = [
     { path: '/home/:id', component: Home },
@@ -18,6 +21,7 @@ const router = createRouter({
 
 const app = createApp(App)
 
+app.use(ElementUI)
 app.use(router)
 
 app.mount('#app')
