68429282031ec5b11fa1d6c0cc09ccff6f3692a0
= <yuleigong0@gmail.com>
2023-01-18 23:01:09 +0800

    remove default, use new; add leftbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import LeftBar from './components/LeftBar.vue'
 </script>
 
 <template>
-  <div>
-    <HelloWorld msg="Hello, Dinner 2023." />
+  <div class="app"> 
+    <!-- <HelloWorld msg="Hello, Dinner 2023." /> -->
+    <LeftBar></LeftBar>
+    <div class="dashboard">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <style scoped>
-
+.app {
+  height: 100vh;
+  background-image: linear-gradient(to top, #c1dfc4 0%, #deecdd 100%);
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+.dashboard{
+    margin-left: 20px;
+    background-color: rgba(204, 182, 182, 0.3);
+    width: 1300px;
+    height: 750px;
+    border-radius: 10px;
+    padding: 10px ;
+}
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,17 +10,32 @@ const count = ref(0)
 
 <template>
   <h1>{{ msg }}</h1>
-  <br>
-  <button>
-    <router-link to="/home/3">Go to Home</router-link>
-  </button>
-  <button>
-    <router-link to="/about">Go to About</router-link>
-  </button>
-  <br>
-  <router-view></router-view>
+  <hr>
+  <LeftBar></LeftBar>
+  <div class="buttons"></div>
+    <button>
+      <router-link to="/home/3">Go to Home</router-link>
+    </button>
+    <button>
+      <router-link to="/about">Go to About!</router-link>
+    </button>
+  <hr>
+  <router-view class="view"></router-view>
 </template>
 
 <style scoped>
+.buttons {
+  display: flex;
+  justify-content: space-around;
+}
 
+.buttons button {
+  font-size: 20px;
+}
+router-view {
+  width: 70%;
+}
+h1 {
+  color: tomato;
+}
 </style>
