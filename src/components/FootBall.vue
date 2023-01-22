7cccebf68c650967b1105f98545bcf35272e81b7
= <yuleigong0@gmail.com>
2023-01-22 14:46:07 +0800

    btn and ElNotification

--- src/components/FootBall.vue
+++ src/components/FootBall.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="movie-item">
+        <div class="upper-column">
+            <span>{{ props.movieInfo.movieName }}</span>
+            <!-- <el-button type="primary" :icon="Edit" plain /> -->
+            <el-button @click="open2" :icon="Delete" plain  color="rgba(79, 75, 75, 0.5)"/>
+        </div>
         <img :src="props.movieInfo.imgId" alt="">
-        <span>{{ props.movieInfo.movieName }}</span>
         <hr>
         <div>
             <p>{{ props.movieInfo.description }}</p>
@@ -11,10 +15,20 @@
 
 <script setup>
 import { ref } from 'vue'
+import { Delete, Edit } from '@element-plus/icons-vue'
+import { h } from 'vue'
+import { ElNotification } from 'element-plus'
 
 const props = defineProps({
     movieInfo: Object
 })
+
+const open2 = () => {
+  ElNotification({
+    title: '删除展示卡片',
+    message: h('i', { style: 'color: teal' }, '卡片已删除...'),
+  })
+}
 </script>
 
 <style scoped>
@@ -22,7 +36,6 @@ const props = defineProps({
     font-size: larger;
     font-weight: bolder;
     border-radius: 5px;
-    /* padding: 20px; */
     margin-top: 10px;
     width: 400px;
     height: 330px;
@@ -45,6 +58,12 @@ const props = defineProps({
     width: 450px;
     z-index: -1;
 }   
+
+.upper-column{
+    display: flex;
+    justify-content: space-between;
+}
+
 
 .movie-item:hover {
     background: rgba(79, 75, 75, 0.5);
