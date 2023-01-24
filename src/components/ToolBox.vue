2154b22b853b6c60675c8ae46d9a590faf7ce409
= <yuleigong0@gmail.com>
2023-01-24 16:59:00 +0800

    clipboard failed

--- src/components/ToolBox.vue
+++ src/components/ToolBox.vue
@@ -1,12 +1,37 @@
 <template>
     <div class="toolbox">
-        <div v-for="i in toolkitArr" class="toolkit">工具{{ i }}</div>
+        <div v-for="i in toolkitArr" class="toolkit">
+            工具{{ i }}
+        <el-input v-model="timeStampShow"/>
+        <el-button type="primary" @click="getTimeStamp">TimeStamp</el-button>
+        <hr>
+        <!-- <el-button type="success" @click="copy">复制</el-button> -->
+        </div>
     </div>
 </template>
 
 <script setup>
-
+import {ref} from 'vue'
+import useClipboard from 'vue-clipboard3'
 const toolkitArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+
+let timeStampShow = ref('')
+
+const getTimeStamp = ()=>{
+    timeStampShow.value = new Date().getTime()/1000
+}
+
+
+const { toClipboard } = useClipboard()
+
+const copy = async() =>{
+    try{
+        await toClipboard(timeStampShow.value)
+        console.log('Copied to clipboard')
+    }catch(e) {
+        alert('fail')
+    }
+}
 
 </script>
 
@@ -31,6 +56,6 @@ const toolkitArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17
     border-radius: 10px;
     background-color: rgba(159, 138, 80, 0.6);
     transition: 0.5s;
-    font-size: 50px;
+    font-size: 10px;
 }
 </style>
